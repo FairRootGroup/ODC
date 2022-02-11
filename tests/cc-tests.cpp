fe98705d1994bf1fce175fbec2eec03b0eb15bb3
Dennis Klein <d.klein@gsi.de>
2022-02-11 02:49:05 +0100

    build(tests): Switch to header-only Boost.UTF

--- tests/cc-tests.cpp
+++ tests/cc-tests.cpp
@@ -7,7 +7,8 @@
  ********************************************************************************/
 
 #define BOOST_TEST_MODULE(odc_custom_commands)
-#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_NO_MAIN
+#define BOOST_TEST_ALTERNATIVE_INIT_API
 #include <boost/test/included/unit_test.hpp>
 
 #include <odc/cc/CustomCommands.h>
@@ -251,3 +252,7 @@ BOOST_AUTO_TEST_CASE(serialization_json)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
+int main(int argc, char* argv[]) {
+    return boost::unit_test::unit_test_main(init_unit_test, argc, argv);
+}
