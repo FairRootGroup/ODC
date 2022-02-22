6f158f24208007e1e2a4f0b9cf686de8aefda25c
Alexey Rybalchenko <alexryba@gmail.com>
2022-02-22 20:21:04 +0100

    Make PluginManager.h header-only.

--- odc/PluginManager.h
+++ odc/PluginManager.h
@@ -9,33 +9,93 @@
 #ifndef __ODC__PluginManager__
 #define __ODC__PluginManager__
 
-// STD
+#include <odc/Def.h>
+#include <odc/Logger.h>
+#include <odc/MiscUtils.h>
+#include <odc/PluginManager.h>
+#include <odc/Process.h>
+
+#include <boost/filesystem.hpp>
+#include <boost/program_options/parsers.hpp>
+
 #include <map>
 #include <memory>
+#include <stdexcept>
 #include <string>
-// ODC
-#include <odc/Def.h>
 
-namespace odc::core
+namespace odc::core {
+
+class CPluginManager
 {
-    class CPluginManager
+  public:
+    // Shared pointer
+    using Ptr_t = std::shared_ptr<CPluginManager>;
+    // Plugin map <plugin name, cmd>
+    using PluginMap_t = std::map<std::string, std::string>;
+
+    void registerPlugin(const std::string& _plugin, const std::string& _cmd)
     {
-      public:
-        // Shared pointer
-        using Ptr_t = std::shared_ptr<CPluginManager>;
-        // Plugin map <plugin name, cmd>
-        using PluginMap_t = std::map<std::string, std::string>;
+        // Check if plugin already registered
+        auto it = m_plugins.find(_plugin);
+        if (it != m_plugins.end()) {
+            std::stringstream ss;
+            ss << "Failed to register resource plugin " << std::quoted(_plugin) << ". A different plugin with the same name is already registered.";
+            throw std::runtime_error(ss.str());
+        }
 
-        void registerPlugin(const std::string& _plugin, const std::string& _cmd);
-        std::string execPlugin(const std::string& _plugin,
-                               const std::string& _resources,
-                               const partitionID_t& _partitionID,
-                               runNr_t _runNr) const;
-        bool isPluginRegistered(const std::string& _plugin) const;
+        // Extract the path from command line
+        std::vector<std::string> args{ boost::program_options::split_unix(_cmd) };
+        std::string path{ args.empty() ? "" : args.front() };
 
-      private:
-        PluginMap_t m_plugins; ///< Plugin map
-    };
+        try {
+            // Throws an exception if path doesn't exist
+            // Check if plugin exists at path and not a directory
+            const boost::filesystem::path pluginPath(boost::filesystem::canonical(boost::filesystem::path(path)));
+            if (boost::filesystem::is_directory(pluginPath)) {
+                std::stringstream ss;
+                ss << "Failed to register resource plugin " << std::quoted(_plugin) << ". Specified path " << pluginPath << " is a directory.";
+                throw std::runtime_error(ss.str());
+            }
+
+            std::string correctedCmd{ _cmd };
+            boost::algorithm::replace_first(correctedCmd, path, pluginPath.string());
+            OLOG(info) << "Register resource plugin " << std::quoted(_plugin) << " as " << std::quoted(correctedCmd);
+            m_plugins.insert(make_pair(_plugin, correctedCmd));
+        } catch (const std::exception& _e) {
+            std::stringstream ss;
+            ss << "Failed to register resource plugin " << std::quoted(_plugin) << ": " << _e.what();
+            throw std::runtime_error(ss.str());
+        }
+    }
+
+    std::string execPlugin(const std::string& _plugin, const std::string& _resources, const partitionID_t& _partitionID, runNr_t _runNr) const
+    {
+        // Check if plugin exists
+        auto it = m_plugins.find(_plugin);
+        if (it == m_plugins.end()) {
+            throw std::runtime_error(toString("Failed to execute resource plugin ", std::quoted(_plugin), ". Plugin not found."));
+        }
+
+        // Execute plugin
+        const std::string cmd{ toString(it->second, " --res ", std::quoted(_resources), " --id ", std::quoted(_partitionID)) };
+        const std::chrono::seconds timeout{ 30 };
+        std::string out;
+        std::string err;
+        int exitCode{ EXIT_SUCCESS };
+        OLOG(debug, _partitionID, _runNr) << "Executing plugin " << std::quoted(cmd);
+        execute(cmd, timeout, &out, &err, &exitCode);
+
+        if (exitCode != EXIT_SUCCESS) {
+            throw std::runtime_error(toString("Plugin ", std::quoted(_plugin), " execution failed with exit code: ", exitCode, "; error message: ", err));
+        }
+        return out;
+    }
+    bool isPluginRegistered(const std::string& _plugin) const { return m_plugins.find(_plugin) != m_plugins.end(); }
+
+  private:
+    PluginMap_t m_plugins; ///< Plugin map
+};
+
 } // namespace odc::core
 
 #endif /* defined(__ODC__PluginManager__) */
