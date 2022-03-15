4b6b63fd8ce1c247e86fca2b166acc9b2a84b289
Alexey Rybalchenko <alexryba@gmail.com>
2022-03-15 20:43:49 +0100

    odc-epn-topo: add nmin parameter

--- examples/odc-epn-topo.cpp
+++ examples/odc-epn-topo.cpp
@@ -15,6 +15,7 @@
 #include <boost/program_options/variables_map.hpp>
 // DDS
 #include <dds/dds.h>
+#include <dds/TopoVars.h>
 
 using namespace std;
 using namespace dds::topology_api;
@@ -53,8 +54,9 @@ int main(int argc, char** argv)
     try {
         string ddTopo;
         vector<string> recoTopos;
-        string monTopo;
+        string monitorTask;
         size_t recoN;
+        size_t recoNmin;
         vector<string> calibTopos;
         string recoWorkerNodes;
         string recoAgentGroup;
@@ -63,21 +65,21 @@ int main(int argc, char** argv)
         string outputTopo;
         string prependExe;
 
-        // Generic options
         bpo::options_description options("odc-epn-topo options");
-        options.add_options()("help,h",       "Produce help message");
-        options.add_options()("dd",           bpo::value<string>(&ddTopo)->default_value(""), "Filepath to XML topology of Data Distribution");
-        options.add_options()("reco,r",       bpo::value<vector<string>>(&recoTopos)->multitoken(), "Space separated list of filepaths of reconstruction XML topologies");
-        options.add_options()("recown",       bpo::value<string>(&recoWorkerNodes)->default_value(""), "Name of the reco worker node");
-        options.add_options()("recogroup",    bpo::value<string>(&recoAgentGroup)->default_value(""), "Name of the reco agent group");
-        options.add_options()("mon",          bpo::value<string>(&monTopo)->default_value(""), "Filepath to XML topology of a stderr monitor tool");
-        options.add_options()("n",            bpo::value<size_t>(&recoN)->default_value(1), "Multiplicator for the reconstruction group");
-        options.add_options()("calib,c",      bpo::value<vector<string>>(&calibTopos)->multitoken(), "Space separated list of filepaths of calibration XML topologies");
-        options.add_options()("calibwn",      bpo::value<string>(&calibWorkerNodes)->default_value(""), "Name of the calibration worker node");
-        options.add_options()("calibgroup",   bpo::value<string>(&calibAgentGroup)->default_value(""), "Name of the calibration agent group");
-        options.add_options()("prependexe,p", bpo::value<string>(&prependExe)->default_value(""), "Prepend with the command all exe tags");
-        options.add_options()("output,o",     bpo::value<string>(&outputTopo)->default_value("topology.xml"), "Output topology filepath");
-        // Parsing command-line
+        options.add_options()
+            ("help,h",       "Produce help message")
+            ("dd",           bpo::value<string>(&ddTopo)->default_value(""),                 "Filepath to XML topology of Data Distribution")
+            ("reco,r",       bpo::value<vector<string>>(&recoTopos)->multitoken(),           "Space separated list of filepaths of reconstruction XML topologies")
+            ("recown",       bpo::value<string>(&recoWorkerNodes)->default_value(""),        "Name of the reconstruction worker node")
+            ("recogroup",    bpo::value<string>(&recoAgentGroup)->default_value(""),         "Name of the reconstruction agent group")
+            ("mon",          bpo::value<string>(&monitorTask)->default_value(""),            "Filepath to XML topology of a stderr monitor tool")
+            ("n",            bpo::value<size_t>(&recoN)->default_value(1),                   "Multiplicator for the reconstruction group")
+            ("nmin",         bpo::value<size_t>(&recoNmin)->default_value(0),                "Minimum number of working reco groups before failing a run")
+            ("calib,c",      bpo::value<vector<string>>(&calibTopos)->multitoken(),          "Space separated list of filepaths of calibration XML topologies")
+            ("calibwn",      bpo::value<string>(&calibWorkerNodes)->default_value(""),       "Name of the calibration worker node")
+            ("calibgroup",   bpo::value<string>(&calibAgentGroup)->default_value(""),        "Name of the calibration agent group")
+            ("prependexe,p", bpo::value<string>(&prependExe)->default_value(""),             "Prepend with the command all exe tags")
+            ("output,o",     bpo::value<string>(&outputTopo)->default_value("topology.xml"), "Output topology filepath");
         bpo::variables_map vm;
         bpo::store(bpo::command_line_parser(argc, argv).options(options).run(), vm);
         bpo::notify(vm);
@@ -108,25 +110,25 @@ int main(int argc, char** argv)
             // Combine all tasks from reco DPL collections to a reco collection
             combineDPLCollections(recoTopos, recoCollection, prependExe);
             // Add stderr monitor task and initialize it from XML topology file
-            if (!monTopo.empty()) {
+            if (!monitorTask.empty()) {
                 auto monTask{ recoCollection->addElement<CTopoTask>("ErrorMonitorTask") };
-                monTask->initFromXML(monTopo);
+                monTask->initFromXML(monitorTask);
                 monTask->setExe(prependExe + monTask->getExe());
             }
             // Add new requirement - one Reco DPL collection per host
-            auto recoR{ recoCollection->addRequirement("RecoInstanceRequirement") };
-            recoR->setRequirementType(CTopoRequirement::EType::MaxInstancesPerHost);
-            recoR->setValue("1");
+            auto recoInstanceRequirement{ recoCollection->addRequirement("RecoInstanceRequirement") };
+            recoInstanceRequirement->setRequirementType(CTopoRequirement::EType::MaxInstancesPerHost);
+            recoInstanceRequirement->setValue("1");
             if (!recoWorkerNodes.empty()) {
                 // Add new worker node name requirement
-                auto recoR2{ recoCollection->addRequirement("RecoWnRequirement") };
-                recoR2->setRequirementType(CTopoRequirement::EType::WnName);
-                recoR2->setValue(recoWorkerNodes);
+                auto recoWnRequirement{ recoCollection->addRequirement("RecoWnRequirement") };
+                recoWnRequirement->setRequirementType(CTopoRequirement::EType::WnName);
+                recoWnRequirement->setValue(recoWorkerNodes);
             } else if (!recoAgentGroup.empty()) {
                 // Add new worker node name requirement
-                auto recoR2{ recoCollection->addRequirement("RecoAgentGroupRequirement") };
-                recoR2->setRequirementType(CTopoRequirement::EType::GroupName);
-                recoR2->setValue(recoAgentGroup);
+                auto recoAgentGroupRequirement{ recoCollection->addRequirement("RecoAgentGroupRequirement") };
+                recoAgentGroupRequirement->setRequirementType(CTopoRequirement::EType::GroupName);
+                recoAgentGroupRequirement->setValue(recoAgentGroup);
             }
         }
 
@@ -138,27 +140,40 @@ int main(int argc, char** argv)
             // Combine all tasks from calobration DPL collections to a calibration collection
             combineDPLCollections(calibTopos, calibCollection, prependExe);
             // Add stderr monitor task and initialize it from XML topology file
-            if (!monTopo.empty()) {
-                auto monTask{ calibCollection->addElement<CTopoTask>("ErrorMonitorTask") };
-                monTask->initFromXML(monTopo);
-                monTask->setExe(prependExe + monTask->getExe());
+            if (!monitorTask.empty()) {
+                auto errorMonitorTask{ calibCollection->addElement<CTopoTask>("ErrorMonitorTask") };
+                errorMonitorTask->initFromXML(monitorTask);
+                errorMonitorTask->setExe(prependExe + errorMonitorTask->getExe());
             }
             if (!calibWorkerNodes.empty()) {
                 // Add new requirement - calibration worker node name
-                auto calibR{ calibCollection->addRequirement("CalibWnRequirement") };
-                calibR->setRequirementType(CTopoRequirement::EType::WnName);
-                calibR->setValue(calibWorkerNodes);
+                auto calibWnRequirement{ calibCollection->addRequirement("CalibWnRequirement") };
+                calibWnRequirement->setRequirementType(CTopoRequirement::EType::WnName);
+                calibWnRequirement->setValue(calibWorkerNodes);
             } else if (!calibAgentGroup.empty()) {
                 // Add new requirement - calibration worker node name
-                auto calibR{ calibCollection->addRequirement("CalibAgentGroupRequirement") };
-                calibR->setRequirementType(CTopoRequirement::EType::GroupName);
-                calibR->setValue(calibAgentGroup);
+                auto calibAgentGroupRequirement{ calibCollection->addRequirement("CalibAgentGroupRequirement") };
+                calibAgentGroupRequirement->setRequirementType(CTopoRequirement::EType::GroupName);
+                calibAgentGroupRequirement->setValue(calibAgentGroup);
             }
         }
 
         // Save topology to the oputput file
         creator.save(outputTopo);
         cout << "New DDS topology successfully created and saved to a file " << quoted(outputTopo) << endl;
+
+        if (recoTopos.size() > 0 && recoN > 0 && recoNmin > 0) {
+            if (recoNmin > recoN) {
+                cerr << "--nmin (" << recoNmin << ") cannot be larger than --n (" << recoN << "), aborting" << endl;
+                return EXIT_FAILURE;
+            }
+
+            CTopoVars vars;
+            vars.initFromXML(outputTopo);
+            // "odc_nmin_" is a convention. ODC will look for this prefix, to map reco collections to their nmin value
+            vars.add("odc_nmin_RecoCollection", std::to_string(recoNmin));
+            vars.saveToXML(outputTopo);
+        }
 
         // Validate created topology
         // Create a topology from the output file
