32e86160aadc77f7c8935c7f53552a09e275e14f
Alexey Rybalchenko <alexryba@gmail.com>
2023-07-14 13:50:56 +0200

    Put detailed device list into debug log

--- odc/grpc/Controller.h
+++ odc/grpc/Controller.h
@@ -455,9 +455,9 @@ class GrpcController final : public odc::ODC::Service
     {
         logGeneralReply(label, common, rep.reply());
         if (!rep.devices().empty()) {
-            OLOG(info, common) << "Detailed list of devices:";
+            OLOG(info, common) << "Detailed list of " << rep.devices().size() << " devices (logged with debug severity):";
             for (const auto& d : rep.devices()) {
-                OLOG(info, common) << "id: "        << d.id()
+                OLOG(debug, common) << "id: "        << d.id()
                                    << "; state: "   << d.state()
                                    << "; path: "    << d.path()
                                    << "; ignored: " << d.ignored()
