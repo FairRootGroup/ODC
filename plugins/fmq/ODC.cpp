26903f6c5352bad897803b8c3ec40975fd0d123e
Alexey Rybalchenko <alexryba@gmail.com>
2024-02-20 14:14:29 +0100

    Capture 'this' in the lambda explicitly

--- plugins/fmq/ODC.cpp
+++ plugins/fmq/ODC.cpp
@@ -235,7 +235,7 @@ void ODC::SubscribeForConnectingChannels()
         string channelName = key.substr(8);
         LOG(info) << "Update for channel name: " << channelName;
 
-        boost::asio::post(fWorkerQueue, [=]() {
+        boost::asio::post(fWorkerQueue, [=, this]() {
             try {
                 {
                     unique_lock<mutex> lk(fUpdateMutex);
